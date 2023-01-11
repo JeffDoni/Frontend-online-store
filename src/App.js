a683f9bb1e3ebdaaa1e046566fd44b887fdf85d0
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-01-11 16:17:56 -0300

    Requisito3/ Carrinho de compras

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/shoppingCart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
