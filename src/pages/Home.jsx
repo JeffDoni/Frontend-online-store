2164bad836dec8c57a48c65624fd9b90f8d8e03d
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-01-11 16:49:26 -0300

    requsito4/ listagem de categorias

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
+  state = {
+    category: [],
+  };
+
+  async componentDidMount() {
+    const list = await getCategories();
+    this.setState({
+      category: list,
+    });
+  }
+
   render() {
+    const { category } = this.state;
     return (
       <div>
+        {category.map((e) => (
+          <form key={ e.id }>
+            <label htmlFor={ e.id } data-testid="category">{e.name}</label>
+            <input
+              type="radio"
+              id={ e.id }
+              value={ e.name }
+            />
+          </form>
+        ))}
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
